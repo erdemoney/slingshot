4aa22371ea3bac53cf11d27d7ee8f86ebe94f1a4
Eric <edemoney@ciena.com>
2024-05-01 12:45:28 -0700

    add module feature and pytest test feature

--- src/slingshot_edemoney/slingshot.py
+++ src/slingshot_edemoney/slingshot.py
@@ -4,7 +4,7 @@ import argparse
 import json
 import os
 import pty
-import snoop
+import sys
 
 import sysrsync
 import git
@@ -56,14 +56,15 @@ def write_config(config_file_path: str, config: dict):
         json.dump(config, file, ensure_ascii=False, indent=4)
 
 
-def update_config(runtime_config: dict, config: dict, args, remote_host: str, local_source_file_path: str) -> dict:
+def update_config(runtime_config: dict, config: dict, args, remote_host: str, local_path: str, module: str | None = None) -> dict:
+    config_key = local_path if module is None else f"{local_path}_mod_{module}"
     try:
         host_cfg = config["script_cfg"][remote_host]
     except KeyError:
         host_cfg = {}
 
     try:
-        script_cfg = config["script_cfg"][remote_host]["scripts"][str(local_source_file_path)]
+        script_cfg = config["script_cfg"][remote_host]["scripts"][str(config_key)]
     except KeyError:
         script_cfg = {}
 
@@ -81,16 +82,16 @@ def update_config(runtime_config: dict, config: dict, args, remote_host: str, lo
         edit_args(runtime_config)
 
     cur = config
-    for key in ["script_cfg", remote_host, "scripts", str(local_source_file_path)]:
+    for key in ["script_cfg", remote_host, "scripts", str(config_key)]:
         if key not in cur:
             cur[key] = {}
         cur = cur[key]
 
     if "args" in runtime_config:
-        config["script_cfg"][remote_host]["scripts"][str(local_source_file_path)]["args"] = runtime_config["args"]
+        config["script_cfg"][remote_host]["scripts"][str(config_key)]["args"] = runtime_config["args"]
 
     if "interpreter" in runtime_config:
-        config["script_cfg"][remote_host]["scripts"][str(local_source_file_path)]["interpreter"] = runtime_config["interpreter"]
+        config["script_cfg"][remote_host]["scripts"][str(config_key)]["interpreter"] = runtime_config["interpreter"]
 
     if runtime_config["auto_add_hosts"] and remote_host not in config["script_cfg"]:
         config["script_cfg"][remote_host] = {}
@@ -125,46 +126,63 @@ def sync_project_to_remote(remote_host: str,
                  destination=str(remote_project_root),
                  destination_ssh=remote_host,
                  options=runtime_config["rsync_options"],
-                 sync_source_contents=False)
+                 sync_source_contents=False,
+                 strict=False)
 
 
 def edit_args(config: dict):
     config["args"] = prompt("args: ", default=config["args"] if "args" in config else "")
 
 
-def execute_on_remote(remote_source_file_path: str, remote_host: str, runtime_config: dict):
+def execute_script_on_remote(remote_source_file_path: str, remote_host: str, runtime_config: dict, test: str | None = None):
+    if test:
+        remote_source_file_path = Path(f"{remote_source_file_path}::{test}")
+
     return pty.spawn(["ssh", "-tt", remote_host,
                       "cd", remote_source_file_path.parent,
                       "&&",
                       runtime_config["interpreter"], str(remote_source_file_path), runtime_config["args"] if "args" in runtime_config else ""])
 
 
-def get_remote_path(local_source_file_path: Path, local_project_root: Path, remote_base_dir: str) -> Path:
-    relative_source_file_path = Path(str(local_source_file_path).replace(str(local_project_root.parent), ""))
+def execute_module_on_remote(remote_dir: str, module_name: str, remote_host: str, runtime_config: dict):
+    print(f"{remote_host=} {remote_dir=} {runtime_config['interpreter']=} {module_name=} ")
+    return pty.spawn(["ssh", "-tt", remote_host,
+                      "cd", remote_dir,
+                      "&&",
+                      runtime_config["interpreter"], "-m", module_name, runtime_config["args"] if "args" in runtime_config else ""])
+
+
+def get_remote_path(local_path: Path, local_project_root: Path, remote_base_dir: str) -> Path:
+    relative_source_file_path = Path(str(local_path).replace(str(local_project_root.parent), ""))
     remote_source_file_path = Path(f"{remote_base_dir}{relative_source_file_path}")
     return remote_source_file_path
 
 
 def get_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument("source_file")
-    parser.add_argument("-r", "--remote_host", action="store_true",
-                        default=None,
+    group = parser.add_mutually_exclusive_group(required=True)
+    group.add_argument("source_file",
+                       nargs="?",
+                       help="source file to execute")
+    group.add_argument("-m",
+                       nargs="?",
+                       help="Module to execute")
+    parser.add_argument("-r", "--remote_host",
+                        action="store_true",
                         help="host to execute code on")
+    parser.add_argument("-t", "--test",
+                        help="pytest test to run")
     parser.add_argument("-a", "--args",
-                        default=None,
                         help="command line args to execute script with")
     parser.add_argument("-i", "--interpreter",
-                        default=None,
                         help="interpreter to execute script with")
     parser.add_argument("-e", "--edit_args",
                         action="store_true",
                         help="edit command line args")
-    parser.add_argument("-p", "--prompt", action="store_true",
-                        default=None,
+    parser.add_argument("-p", "--prompt",
+                        action="store_true",
                         help="prompt for entry")
     parser.add_argument("-v", "--verbose",
-                        default=None,
                         action="store_true",
                         help="verbose")
 
@@ -177,23 +195,26 @@ def main():
     config, config_file_path = read_config()
     runtime_config = DEFAULT_CONFIG | config["global"]
 
-    local_source_file_path = Path(args.source_file).absolute()
-    assert os.path.exists(args.source_file)
+    local_path = Path(args.source_file).absolute() if args.source_file else Path().resolve().absolute()
+    if not local_path.exists():
+        sys.exit(f"File `{local_path}` does not exist")
 
-    if args.remote_host is not None:
+    if args.remote_host:
         remote_host = args.remote_host
-    elif args.prompt is not None:
+    elif args.prompt:
         remote_host = select_remote_host(config["script_cfg"])
     else:
         remote_host = config["mru_interpreter"]
 
+    # NOTE: mru_interpreter should be mru_host
     config["mru_interpreter"] = remote_host
 
     runtime_config = update_config(runtime_config=runtime_config,
                                    config=config,
                                    args=args,
                                    remote_host=remote_host,
-                                   local_source_file_path=local_source_file_path)
+                                   local_path=local_path,
+                                   module=args.m)
 
     local_project_root = find_project_root(file=args.source_file, project_roots=runtime_config["project_roots"])
     remote_project_root = runtime_config["remote_base_dir"]
@@ -206,13 +227,20 @@ def main():
                            remote_project_root=remote_project_root,
                            runtime_config=runtime_config)
 
-    remote_source_file_path = get_remote_path(local_project_root=local_project_root,
-                                              local_source_file_path=local_source_file_path,
-                                              remote_base_dir=runtime_config["remote_base_dir"])
+    remote_path = get_remote_path(local_project_root=local_project_root,
+                                  local_path=local_path,
+                                  remote_base_dir=runtime_config["remote_base_dir"])
 
-    execute_on_remote(remote_source_file_path=remote_source_file_path,
-                      remote_host=remote_host,
-                      runtime_config=runtime_config)
+    if args.m:
+        execute_module_on_remote(remote_dir=remote_path,
+                                 remote_host=remote_host,
+                                 runtime_config=runtime_config,
+                                 module_name=args.m)
+    else:
+        execute_script_on_remote(remote_source_file_path=remote_path,
+                                 remote_host=remote_host,
+                                 runtime_config=runtime_config,
+                                 test=args.test)
 
 
 if __name__ == "__main__":
